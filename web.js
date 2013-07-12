e9364287e04d601ee28571605b46709fa928a65b
Ubuntu <ubuntu@ip-10-212-86-77.ec2.internal>
2013-07-12 02:31:07 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,10 +3,17 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+    var fs = require('fs');
+    var buf =fs.readFileSynch('index.html','utf8');
+    response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+   console.log("Listening on " + port);
 });
+
+
+
+
